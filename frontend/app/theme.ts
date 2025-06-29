d0d50aa70e8eadb76551c33f7d505a17b9dc84a0
Josh <joshua.oyekunle10@gmail.com>
2025-06-29 12:10:32 +0100

    chore: add primary, secondary colour change

--- frontend/app/theme.ts
+++ frontend/app/theme.ts
@@ -1,9 +1,17 @@
-'use client';
-import { createTheme } from '@mui/material/styles';
+"use client";
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
-    fontFamily: 'var(--font-roboto)',
+    fontFamily: "var(--font-roboto)",
+  },
+  palette: {
+    primary: {
+      main: "#013220",
+    },
+    secondary: {
+      main: "#d9ead3",
+    },
   },
 });
 
