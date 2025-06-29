146ea0661525e3412b5822a5ea11ba13e1433785
Viibrant <11376396+Viibrant@users.noreply.github.com>
2025-06-29 10:37:14 +0000

    feat(frontend): plumb in pins from supabase

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,44 +2,50 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/**",
       },
 
       {
-        protocol: 'https',
-        hostname: 'external-content.duckduckgo.com',
-        port: '',
-        pathname: '/iu/**',
+        protocol: "https",
+        hostname: "external-content.duckduckgo.com",
+        port: "",
+        pathname: "/iu/**",
       },
       {
-        protocol: 'https',
-        hostname: 'jwjeuqbkksddragbdbue.supabase.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "jwjeuqbkksddragbdbue.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jwjeuqbkksddragbdbue.supabase.co",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:8000/api/:path*",
       },
     ];
   },
-
 };
 
 export default nextConfig;
